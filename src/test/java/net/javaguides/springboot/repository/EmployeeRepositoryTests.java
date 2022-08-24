5beec84cf426fc46a8f2d337b4828ecc8d145bb0
Hisuji <sujikim.dev@gmail.com>
2022-08-25 00:09:54 +0900

    Test : Unit test for get all employees operation

--- src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTests.java
+++ src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTests {
 
@@ -32,4 +34,41 @@ public class EmployeeRepositoryTests {
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
     }
+
+    // Junit test for get all employees operation
+    @DisplayName("Junit test for get all employees operation")
+    @Test
+    public void givenEmployeeList_whenFindAll_thenEmployeeList() {
+        // given - precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("Hazel")
+                .firstName("Hazel")
+                .lastName("Tree")
+                .email("HazelTree@coffee.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Coffee")
+                .lastName("Latte")
+                .email("CoffeeLatte@coffee.com")
+                .build();
+
+        Employee employee3 = Employee.builder()
+                .firstName("Candy")
+                .lastName("Sweet")
+                .email("CandySweet@coffee.com")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
+        // when - action or the behaviour that we are going test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(3);
+    }
+
+
 }
