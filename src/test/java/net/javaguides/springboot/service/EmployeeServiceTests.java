769a33a7c5b4972ba5105c3fa9ab164ee4ad2804
Hisuji <sujikim.dev@gmail.com>
2022-09-21 22:42:26 +0900

    test : Using @Mock and @InjectMocks annotations to mock the object

--- src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
@@ -7,17 +7,27 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class) // 주석 없으면 NullPointerException 발생 :  테스트 클래스가 Mockito를 사용한다는 것을 알림
 public class EmployeeServiceTests {
 
-    private EmployeeRepository employeeRepository;
-    private EmployeeService employeeService;
+    @Mock // We can use @Mock to create and inject mocked instances without having to call Mockito.mock manually
+    private EmployeeRepository employeeRepository; // @Mock 어노테이션 사용 -> employeeRepository = mock(EmployeeRepository.class); 주석처리
+
+//    private EmployeeService employeeService; // 주석 -> employeeService = new EmployeeServiceImpl(employeeRepository); 주석처리
+    @InjectMocks // @InjectMocks creates the mock object of the class and injects the mocks that are marked with the annotations @Mock into it.
+    private EmployeeServiceImpl employeeService;
+
+    private Employee employee;
 
     @BeforeEach // this method would be basically executed before each JUnit test case within this class.
     public void setup() {
@@ -26,9 +36,16 @@ public class EmployeeServiceTests {
         // using Mockito.mock() method - import org.mockito.Mockito;
 //        employeeRepository = Mockito.mock(EmployeeRepository.class);
 
-        employeeRepository = mock(EmployeeRepository.class); // import static org.mockito.Mockito.*;
+//        employeeRepository = mock(EmployeeRepository.class); // import static org.mockito.Mockito.*;
 
-        employeeService = new EmployeeServiceImpl(employeeRepository);  // 생성자 기반 종속성 주입
+//        employeeService = new EmployeeServiceImpl(employeeRepository);  // 생성자 기반 종속성 주입
+
+         employee = Employee.builder()
+                .id(1L)
+                .firstName("Hazel")
+                .lastName("Tree")
+                .email("HazelTree@coffee.com")
+                .build();
     }
 
     // Junit test for saveEmployee method
@@ -36,17 +53,11 @@ public class EmployeeServiceTests {
     @Test
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
         // given - precondition or setup
-        Employee employee = Employee.builder()
-                .id(1L)
-                .firstName("Hazel")
-                .lastName("Tree")
-                .email("HazelTree@coffee.com")
-                .build();
-        
         //saveEmployee() 메서드 > findByEmail 테스트
-        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty()); // Optional.empty() : 비어있는(null) Optional 객체를 가져온다.
+        //willReturn() 반환하는 값
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty()); // Optional.empty() : 비어있는(null) Optional 객체를 가져온다.
         //saveEmployee() 메서드 > save() 테스트
-        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+        given(employeeRepository.save(employee)).willReturn(employee);
 
         System.out.println(employeeRepository);
         System.out.println(employeeService);
