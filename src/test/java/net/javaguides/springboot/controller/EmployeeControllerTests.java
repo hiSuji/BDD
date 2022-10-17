5e6ae167e8af083ceebee6f8904b513777ab9b5e
Hisuji <sujikim.dev@gmail.com>
2022-10-18 07:22:53 +0900

    test : Unit test createEmployee REST API

---
+++ src/test/java/net/javaguides/springboot/controller/EmployeeControllerTests.java
@@ -0,0 +1,64 @@
+package net.javaguides.springboot.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.service.EmployeeService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import static org.mockito.BDDMockito.given;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+// @WebMvcTest annotation will load only the spring beans that are required to test the controller.
+// It won't load the service layer components or the repository component.
+@WebMvcTest
+public class EmployeeControllerTests {
+    @Autowired
+    private MockMvc mockMvc; // MockMvc to call REST API.
+
+    // @MockBean annotation will tell spring that create this mock object.
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper; // ObjectMapper Jackson class to serialize and decentralize java objects.
+
+    // Junit test for
+    @Test
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Banana")
+                .lastName("Kim")
+                .email("banana@gmail.com")
+                .build();
+
+        // ArgumentMatchers : Allows creating customized argument matchers.
+        // any() → Matches anything (null values)
+        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0)); // in this case we only have one parameter, it is on place 0
+
+        // when - action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/employees") // post REST API 호출
+                .contentType(MediaType.APPLICATION_JSON) // Json type
+                .content(objectMapper.writeValueAsString(employee))); // 본문에 employee JSON 객체 전달 (writeValueAsString : Java 객체에서 JSON 생성하고 생성된 JSON -> 문자열로 반환)
+
+        // then - verify the output
+        // jsonPath - ($ : The root element to query. This starts all path expressions.)
+        response.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+
+    }
+}
