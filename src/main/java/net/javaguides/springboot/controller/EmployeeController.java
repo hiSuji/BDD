cc7033460a7e8498ae3a6f1ff2132f0dfc57b345
Hisuji <sujikim.dev@gmail.com>
2022-10-21 05:31:57 +0900

    feat : Unit test createEmployee REST API

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -5,13 +5,15 @@ import net.javaguides.springboot.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 // @RestController annotation to make this class as a spring MVC controller.
 @RestController
 // @RequestMapping annotation to define URL for all the REST APIs in this controller.
 @RequestMapping("/api/employees")
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     // spring 4.3 버전 이후 spring IOC finds a spring bean with a single constructor
     // spring IOC will automatically inject this dependency.
@@ -28,4 +30,7 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() { return employeeService.getAllEmployees(); }
 }
